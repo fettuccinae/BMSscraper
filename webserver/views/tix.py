a7c8a510cbdf98b7335d47d9d74c7cb43dd347c2
junaid <shaikjunaid323@gmail.com>
2025-10-15 06:33:08 +0000

    fbg

--- webserver/views/tix.py
+++ webserver/views/tix.py
@@ -106,10 +106,10 @@ def cron():
 
         app = current_app._get_current_object()
         def run_the_slow_part_in_context():
-            with app.app_context():
+            with app.app_context(tix_urls, movie_urls):
                 _the_slow_part(tix_urls, movie_urls)
 
-        thread = threading.Thread(target=run_the_slow_part_in_context)
+        thread = threading.Thread(target=run_the_slow_part_in_context, args=(tix_urls, movie_urls))
         thread.start()
 
         return jsonify({"status": "aight"})
